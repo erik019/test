7601f2458f89fa5a07d4678abf494c52cc0c8891
erik019 <undle40@gmail.com>
2017-08-24 09:30:39 -0600

    ni idea

---
+++ www/js/app.js
@@ -0,0 +1,32 @@
+var base = 7 ;
+var altura = 5;
+var area = base + altura;
+console.log("el area de mi rectangulo es igual:" + area)
+
+generar el area de un circulo, el area de un triangulo y el volumen de una esfera, 
+el resultado mostrarlo en comentario de la consola
+
+circulo
+
+var pi = 3.1416;
+var radio = 4;
+var cuad = radio * radio;
+var res = pi * cuad;
+console.log("el area de mi circulo es igual: " + res)
+
+triangulo
+
+var base = 3;
+var altura = 4;
+var cuad = base * altura;
+var res = cuad / 2;
+console.log("el area de mi triangulo es igual: " + res)
+
+esfera
+
+var pi = 3.1416;
+var radio = 4;
+var cub = radio * radio * radio;
+var res1 = pi * cub * 4;
+var res = res1 / 3;
+console.log("el volumen de mi esfera es igual: " + res)
